72191cf0b3567e49527c4aa5538a75167533a55c
Deathgrind <111877127+Deathgrint@users.noreply.github.com>
2024-08-20 22:28:19 +0200

    Add files via upload

--- installimage-proxmox.sh
+++ installimage-proxmox.sh
@@ -461,7 +461,7 @@ if [ "$OS" == "PBS" ] ; then
   postinstall_file="/root/pbs"
 else
   if [ ! -f postinstall_file="/root/pve" ] ; then
-    wget "https://raw.githubusercontent.com/extremeshok/xshok-proxmox/master/hetzner/pve" -c -O /root/pve
+    wget "https://raw.githubusercontent.com/Deathgrint/Hetzner-Server/main/pve" -c -O /root/pve
   fi
   postinstall_file="/root/pve"
 fi
